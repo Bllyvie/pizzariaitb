3da712b9879282b38a116874e383103537c9e399
Bllyvie <be_bebelly@outlook.com>
2024-08-11 20:31:18 -0300

    tudo acabará em pizza.

--- src/main.jsx
+++ src/main.jsx
@@ -7,5 +7,6 @@ import { BrowserRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
+export default App;
